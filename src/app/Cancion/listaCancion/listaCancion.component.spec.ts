f4cb9ebb8265bbd38c58ec802afb4dc6f75adfaf
juancamilobonet2 <j.bonet@uniandes.edu.co>
2022-05-10 23:56:54 -0500

    a

--- src/app/Cancion/listaCancion/listaCancion.component.spec.ts
+++ src/app/Cancion/listaCancion/listaCancion.component.spec.ts
@@ -12,7 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListaCancionComponent } from './listaCancion.component';
 import { CancionService } from '../cancion.service';
 import { Cancion } from '../cancion';
-import { debug } from 'console';
 
 describe('ListaCancionComponent', () => {
  let component: ListaCancionComponent;
@@ -32,24 +31,13 @@ describe('ListaCancionComponent', () => {
    fixture = TestBed.createComponent(ListaCancionComponent);
    component = fixture.componentInstance;
 
-   
-   component.canciones = [
-     new Cancion(faker.lorem.sentence(),
-     faker.datatype.number(),
-     ),
-   ];
+
    fixture.detectChanges();
    debug = fixture.debugElement;
  });
 
  it('should create', () => {
    expect(component).toBeTruthy();
- });
-
- it('should have an img element ', () => {
-   expect(debug.query(By.css('img')).attributes['alt']).toEqual(
-     component.capitulos[0].titulo
-   );
  });
 
 });
