c6a9f086f754c95dbb93d446cd23341bc13eab7b
juancamilobonet2 <j.bonet@uniandes.edu.co>
2022-05-24 01:23:39 -0500

    avances en diseno

--- src/app/album/album-detail.ts
+++ src/app/album/album-detail.ts
@@ -1,23 +1,24 @@
 import { Album } from "./album";
 import { Genero } from "../genero/genero";
+import { Creador } from "../Creador/creador";
+import { Cacion } from "../cacion/cacion";
 
 export class AlbumDetail extends Album {
-  //TODO relaciones
-  //artistas: Array<Artista> = [];
-  //canciones: Array<Cancion> = [];
+  artistas: Array<Creador> = [];
+  canciones: Array<Cacion> = [];
   generos: Array<Genero> = [];
 
   constructor(
     id: number,
     titulo: string,
     imagen: string,
-    //artistas: Array<Artista>,
-    //canciones: Array<Cancion>,
+    artistas: Array<Creador>,
+    canciones: Array<Cacion>,
     generos: Array<Genero>
   ) {
     super(id, titulo, imagen);
-    //this.artistas = artistas;
-    //this.canciones = canciones;
+    this.artistas = artistas;
+    this.canciones = canciones;
     this.generos = generos;
   }
 }
--- src/app/album/album-list/album-list.component.ts
+++ src/app/album/album-list/album-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlbumService } from '../album.service';
 import { AlbumDetail } from '../album-detail';
+import { Creador } from 'src/app/Creador/creador';
 
 @Component({
   selector: 'app-album-list',
@@ -30,6 +31,20 @@ export class AlbumListComponent implements OnInit {
   ngOnInit() {
     this.getAlbums();
     console.log(this.albums);
+  }
+
+  generateArtistsText(album:AlbumDetail):string{
+    var artists: Array<Creador> = album.artistas;
+    var text: string = "";
+    if(artists.length>3){
+      text = "Varios Artistas";
+    } else {
+      for(var i:number =0; i<(artists.length-1);i++){
+        text+=artists[i].nombre + ", ";
+      }
+      text+=artists[artists.length-1].nombre;
+    }
+    return text;
   }
 
 }
